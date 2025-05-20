16abe80e07ada4a33c1a2c7521131402ad51eb77
pablo10diez2 <pablo.diez@opendeusto.es>
2025-05-20 14:45:04 +0200

    circle implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "config.h"
 #include <iostream>
-#include <cmath>
 #include <ctime>
 #include <cmath>
+#include <glm/glm.hpp>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,9 @@ unsigned int make_module(const std::string& filepath, unsigned int module_type);
 unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath);
 void gravity(float* vertices, int numParameters, int numTriangles, float* speed);
 void isEdge(float* vertices, int numParameters, int numTriangles, float* speed);
+void buildCircle(float radius, int count);
+
+std::vector<float> vertices;
 
 int main(){
     GLFWwindow* window;
@@ -30,30 +34,9 @@ int main(){
 
     unsigned int shader = make_shader("../src/shaders/vertex.txt", "../src/shaders/fragment.txt");
     
-    int numTriangles = 3;
-    int numParameters = 6;
+    int numTriangles = 12;
 
-    int verticesSpace = numTriangles * 18;
-
-    float* vertices = new float[18*numTriangles] {
-            -0.5f, 0.9f, 0.0f,  1.0f, 0.0f, 0.0f,
-            -0.6f, 0.5f, 0.0f,   0.0f, 1.0f, 0.0f,
-            -0.4f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,
-
-            0.2f, 0.75f, 0.0f,   1.0f, 0.0f, 0.0f,
-            0.2f, 0.2f, 0.0f,   0.0f, 1.0f, 0.0f,
-            0.8f,  0.2f, 0.0f,   0.0f, 0.0f, 1.0f,
-            
-            0.2f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,
-            0.0f, 0.2f, 0.0f,   0.0f, 0.0f, 1.0f
-        };
-
-    float* speeds = new float[numTriangles]{
-        -0.0001f,
-        -0.0001f,
-        -0.0001f
-    };
+    buildCircle(0.2, numTriangles);
 
     unsigned int VBO, VAO;
 
@@ -62,7 +45,7 @@ int main(){
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, verticesSpace * sizeof(float), vertices, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*6*vertices.size(), &vertices[0], GL_DYNAMIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -70,28 +53,24 @@ int main(){
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    glBindVertexArray(0);
-
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
         
         //float time = glfwGetTime();   
 
-        isEdge(vertices, numParameters, numTriangles, speeds);
+        //isEdge(vertices2, numParameters, numTriangles, speeds2);
 
-        gravity(vertices, numParameters, numTriangles, speeds);
-        
+        //gravity(vertices2, numParameters, numTriangles, speeds2);
+
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, verticesSpace * sizeof(float), vertices);
-        
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(float)*6*vertices.size(), &vertices[0]);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shader);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 9);
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
         glfwSwapBuffers(window);
-
     }
-    
+
     glDeleteProgram(shader);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
@@ -100,18 +79,47 @@ int main(){
     return 0;
 }
 
-void isEdge(float* vertices, int numParameters, int numTriangles, float* speed){
+void buildCircle(float radius, int count){
+    float angle = 360.0f/count;
+    int triangleCount = count-2;
 
+    std::vector<glm::vec3> temp;
+
+    for(int i=0; i<count; i++){
+        float currentAngle = angle*i;
+        float x = radius * cos(glm::radians(currentAngle));
+        float y = radius * sin(glm::radians(currentAngle));
+        float z = 0.0f;
+
+        temp.push_back(glm::vec3(x,y,z));
+
+    }
+    for(int i=0; i < triangleCount; i++){
+        glm::vec3 v0 = temp[0];
+        glm::vec3 v1 = temp[i+1];
+        glm::vec3 v2 = temp[i+2];
+
+        vertices.insert(vertices.end(), {v0.x, v0.y, v0.z, 0.5f, 1.0f, 0.0f});
+        vertices.insert(vertices.end(), {v1.x, v1.y, v1.z, 0.5f, 1.0f, 0.0f});
+        vertices.insert(vertices.end(), {v2.x, v2.y, v2.z, 0.5f, 1.0f, 0.0f});
+    }
+    for(int i=0; i<12*3; i++){
+        std::cout<<vertices[i]<<std::endl;
+    }
+    
+}
+
+void isEdge(float* vertices, int numParameters, int numTriangles, float* speed){
     for(int i = 0; i<numTriangles; i++){
         for(int z = 1 + (18*i); z < (i*18)+(numParameters*3) +1; z+=6){
             if(vertices[z] <= -1.0f){
-                speed[i] = abs(speed[i]*0.65f);
+                speed[i] = abs((speed[i])*0.65f);
                 if(speed[i]<0.0001f){
                     speed[i] = 0.0f;
                 }
+            }
         }
     }
-}
 }
 
 unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath){
